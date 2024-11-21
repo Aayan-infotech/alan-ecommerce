260646580a342200838867346ea86a6659bd281c
abinash <development.aayanindia@gmail.com>
2024-11-21 10:32:34 +0530

    'laidfn'

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -30,15 +30,18 @@ import bifold_image from "../../assets/card_img4.png";
 import interriordoors_image from "../../assets/interriordoors.png";
 import harware_image from "../../assets/harware.png";
 import catimage from "../../assets/7xm155932.png";
+import Loader from '../../loader/Loader'
 
 export const Home = () => {
   const [exploreCategories, setExploreCategories] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const fetchExploreCategories = async () => {
+    setLoading(true);
     try {
       const response = await axios.get("http://44.196.192.232:5000/api/category");
       if (response?.data?.status === 200) {
@@ -49,6 +52,8 @@ export const Home = () => {
       }
     } catch (error) {
       setErrorMessage(error?.response?.data?.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -144,224 +149,226 @@ export const Home = () => {
   };
 
   return (
-    <div className="home-container px-4">
-      <Container>
-        <Grid container alignItems="center" className="home-content">
-          {/* Left Section: Text */}
-          <Grid item xs={12} md={6} className="text-section">
-            <Typography variant="h3" component="h1" className="main-heading">
-              Upgrade Your Home With
-            </Typography>
-            <Typography
-              variant="h3"
-              component="h1"
-              className="highlighted-heading"
-            >
-              The Right <span>Windows, Doors,</span>{" "}
-              <span style={{ fontWeight: "bold" }}>And More—Affordably.</span>
-            </Typography>
-            <Link to="/get-estimation">
-              <Button
-                variant="contained"
-                color="primary"
-                className="estimate-button"
-              >
-                Get Estimates
-              </Button>
-            </Link>
-          </Grid>
-
-          {/* Right Section: Image */}
-          <Grid item xs={12} md={6} className="image-section">
-            <img
-              src={banner} // Replace with your image path
-              alt="Home"
-              className="home-image"
-            />
-          </Grid>
-        </Grid>
-      </Container>
-      <Box className="marquee-container">
-        <div className="marquee-content" ref={marqueeRef}>
-          <div className="d-flex">
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-          </div>
-          <div className="d-flex">
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-            <Typography
-              variant="h2"
-              className="me-2 fw-bold"
-              sx={{ color: "#0068B3" }}
-            >
-              SELECT PRODUCTS BELOW AND GET PRICING NOW,
-            </Typography>
-          </div>
-        </div>
-      </Box>
-      {/* ----------------all content pages--------------------- */}
-      <Container>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            flexDirection: { xs: "column", md: "row" },
-          }}
-        >
-          {buttonData.map((button, index) => (
-            <Link to={button?.urlPath} key={index}>
-              <Button
-                variant="contained"
-                className="me-3"
-                sx={{ textTransform: "none" }}
-              >
-                {button.label} <ArrowForwardIcon className="fs-5" />
-              </Button>
-            </Link>
-          ))}
-        </Box>
-
-        {/* ----------------Spotlight Deals--------------------- */}
-
-        <Box className="spotlight_deals mt-4 mb-4">
-          <Typography variant="h4" className="heading_1">
-            Explore
-          </Typography>
-        </Box>
-
-        <Grid container spacing={2} justifyContent="center" alignItems="center">
-          {exploreCategories.length > 0 ? (
-            exploreCategories.map((item, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card
-                  sx={{ maxWidth: 300, mx: "auto" }}
-                  onClick={() => handleClick(item?._id)}
-                >
-                  <CardMedia
-                    component="img"
-                    height="300"
-                    image={item?.image || No_Image_Available}
-                    alt="green iguana"
-                    sx={{ objectFit: "fill" }}
-                  />
-                  <CardContent sx={{ backgroundColor: "#0068B333" }}>
-                    <Box
-                      sx={{
-                        color: "#0068B3",
-                        fontWeight: "bold",
-                      }}
-                      className="text-center"
-                    >
-                      <Typography
-                        className="fw-bold"
-                        variant="h5"
-                        component="div"
-                      >
-                        {item?.categoryName
-                          ?.split(' ')
-                          .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-                          .join(' ')}
-                      </Typography>
-                    </Box>
-                    <Box className="mt-2">
-                      <Button
-                        size="large"
-                        variant="contained"
-                        sx={{ textTransform: "none", fontWeight: "bold" }}
-                        className="w-100 fw-bold"
-                      >
-                        Buy Now
-                      </Button>
-                    </Box>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))
-          ) : (
-            <Grid item xs={12}>
-              <Typography variant="h6" align="center">
-                {errorMessage}
+    <>
+      <div className="home-container px-4">
+        <Container>
+          <Grid container alignItems="center" className="home-content">
+            {/* Left Section: Text */}
+            <Grid item xs={12} md={6} className="text-section">
+              <Typography variant="h3" component="h1" className="main-heading">
+                Upgrade Your Home With
               </Typography>
+              <Typography
+                variant="h3"
+                component="h1"
+                className="highlighted-heading"
+              >
+                The Right <span>Windows, Doors,</span>{" "}
+                <span style={{ fontWeight: "bold" }}>And More—Affordably.</span>
+              </Typography>
+              <Link to="/get-estimation">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className="estimate-button"
+                >
+                  Get Estimates
+                </Button>
+              </Link>
             </Grid>
-          )}
-        </Grid>
 
-        {/* ----------------Our Products--------------------- */}
-        <Box>
-          <OurProducts />
+            {/* Right Section: Image */}
+            <Grid item xs={12} md={6} className="image-section">
+              <img
+                src={banner} // Replace with your image path
+                alt="Home"
+                className="home-image"
+              />
+            </Grid>
+          </Grid>
+        </Container>
+        <Box className="marquee-container">
+          <div className="marquee-content" ref={marqueeRef}>
+            <div className="d-flex">
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+            </div>
+            <div className="d-flex">
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+              <Typography
+                variant="h2"
+                className="me-2 fw-bold"
+                sx={{ color: "#0068B3" }}
+              >
+                SELECT PRODUCTS BELOW AND GET PRICING NOW,
+              </Typography>
+            </div>
+          </div>
         </Box>
+        {/* ----------------all content pages--------------------- */}
+        <Container>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: { xs: "column", md: "row" },
+            }}
+          >
+            {buttonData.map((button, index) => (
+              <Link to={button?.urlPath} key={index}>
+                <Button
+                  variant="contained"
+                  className="me-3"
+                  sx={{ textTransform: "none" }}
+                >
+                  {button.label} <ArrowForwardIcon className="fs-5" />
+                </Button>
+              </Link>
+            ))}
+          </Box>
 
-        {/* ----------------Our Customer Feedback--------------------- */}
-        <Box>
-          <CustomerFeedback />
-        </Box>
+          {/* ----------------Spotlight Deals--------------------- */}
 
-        {/* ----------------Latest Blogs--------------------- */}
-        <Box>
-          <LatestBlogs />
-        </Box>
-      </Container>
-    </div>
+          <Box className="spotlight_deals mt-4 mb-4">
+            <Typography variant="h4" className="heading_1">
+              Explore
+            </Typography>
+          </Box>
+
+          <Grid container spacing={2} justifyContent="center" alignItems="center">
+            {exploreCategories.length > 0 ? (
+              exploreCategories.map((item, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Card
+                    sx={{ maxWidth: 300, mx: "auto" }}
+                    onClick={() => handleClick(item?._id)}
+                  >
+                    <CardMedia
+                      component="img"
+                      height="300"
+                      image={item?.image || No_Image_Available}
+                      alt="green iguana"
+                      sx={{ objectFit: "fill" }}
+                    />
+                    <CardContent sx={{ backgroundColor: "#0068B333" }}>
+                      <Box
+                        sx={{
+                          color: "#0068B3",
+                          fontWeight: "bold",
+                        }}
+                        className="text-center"
+                      >
+                        <Typography
+                          className="fw-bold"
+                          variant="h5"
+                          component="div"
+                        >
+                          {item?.categoryName
+                            ?.split(' ')
+                            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+                            .join(' ')}
+                        </Typography>
+                      </Box>
+                      <Box className="mt-2">
+                        <Button
+                          size="large"
+                          variant="contained"
+                          sx={{ textTransform: "none", fontWeight: "bold" }}
+                          className="w-100 fw-bold"
+                        >
+                          Buy Now
+                        </Button>
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            ) : (
+              <Grid item xs={12}>
+                <Typography variant="h6" align="center">
+                  {errorMessage}
+                </Typography>
+              </Grid>
+            )}
+          </Grid>
+
+          {/* ----------------Our Products--------------------- */}
+          <Box>
+            <OurProducts />
+          </Box>
+
+          {/* ----------------Our Customer Feedback--------------------- */}
+          <Box>
+            <CustomerFeedback />
+          </Box>
+
+          {/* ----------------Latest Blogs--------------------- */}
+          <Box>
+            <LatestBlogs />
+          </Box>
+        </Container>
+      </div>
+    </>
   );
 };
